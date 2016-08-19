7e7e7d9929113b70387eb648a63537bc5527bc4f
Silence <374619540@qq.com>
2016-08-19 21:21:04 +0800

    ios9 新特性storyboardReference 简单使用

---
+++ storyboardReference/storyboardReference/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  storyboardReference
+//
+//  Created by 杨晴贺 on 8/19/16.
+//  Copyright © 2016 silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
