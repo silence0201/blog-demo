899ffa602a7c1e36fd784efa7830b70c719ed14b
Silence <374619540@qq.com>
2017-01-30 17:14:58 +0800

    Update

--- Advanced/XCTestDemo/UITests/UITestDemo.m
+++ Advanced/XCTestDemo/UITests/UITestDemo.m
@@ -1,34 +1,34 @@
 //
-//  UITests.m
-//  UITests
+//  UITestDemo.m
+//  XCTestDemo
 //
-//  Created by 杨晴贺 on 26/01/2017.
+//  Created by 杨晴贺 on 30/01/2017.
 //  Copyright © 2017 Silence. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface UITests : XCTestCase
+@interface UITestDemo : XCTestCase
 
 @end
 
-@implementation UITests
+@implementation UITestDemo
 
-/// 方法在XCTestCase的测试方法调用之前调用，可以在测试之前创建在test case方法中需要用到的一些对象等
 - (void)setUp {
     [super setUp];
+    
     self.continueAfterFailure = NO;
     [[[XCUIApplication alloc] init] launch];
 }
 
-/// 当测试全部结束之后调用tearDown方法，法则在全部的test case执行结束之后清理测试现场，释放资源删除不用的对象等
 - (void)tearDown {
     [super tearDown];
 }
 
-- (void)testUI {
-    XCUIApplication *app = [[XCUIApplication alloc] init];
+/// 利用录制生成代码
+- (void)testLogin{
     
+    XCUIApplication *app = [[XCUIApplication alloc] init];
     XCUIElementQuery *nameElementsQuery = [app.otherElements containingType:XCUIElementTypeStaticText identifier:@"Name:"];
     XCUIElement *textField = [[nameElementsQuery childrenMatchingType:XCUIElementTypeTextField] elementBoundByIndex:0];
     [textField tap];
@@ -36,10 +36,11 @@
     
     XCUIElement *textField2 = [[nameElementsQuery childrenMatchingType:XCUIElementTypeTextField] elementBoundByIndex:1];
     [textField2 tap];
+    [textField2 tap];
     [textField2 typeText:@"1234"];
-    
-    [[[[[[XCUIApplication alloc] init] childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element tap];
     [app.buttons[@"Login"] tap];
+    
 }
+
 
 @end
