c27750985dc61e61919c866909fe8e0155ae1529
Silence <374619540@qq.com>
2017-05-02 19:06:27 +0800

    Update

--- Reactive/ReactiveCocoaSwiftDemo/ReactiveCocoaDemo/ViewController.swift
+++ Reactive/ReactiveCocoaSwiftDemo/ReactiveCocoaDemo/ViewController.swift
@@ -11,25 +11,100 @@ import ReactiveCocoa
 import ReactiveSwift
 import Result
 
-enum MyError: Error {
-    case TestError
+class Person: NSObject {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
 }
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var person: Person = Person(name: "Test")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        operators()
-        mapDemo()
-        filterDemo()
-        takeDemo()
-        signalProducerDemo()
-        replayLazilyDemo()
-        logEvents()
+//        operators()
+//        mapDemo()
+//        filterDemo()
+//        takeDemo()
+//        signalProducerDemo()
+//        replayLazilyDemo()
+//        logEvents()
+//
+//        let (signal,observer) = Signal<String,NoError>.pipe()
+//        let uniqueSignal = signal.uniqueValues()
+//        observer.send(value: "12")
+//        observer.send(value: "14")
+//        
+//        label.reactive.text <~ uniqueSignal
+//        observer.send(value: "414")
+        
+        createSignalMehods()
+        textFieldDemo()
+        
+    }
+    // MARK: - 信号创建
+    func createSignalMehods() {
+        // 通过信号发生器创建--冷信号
+        let producter = SignalProducer<String,NoError>.init { (observe, _) in
+            print("新的订阅，启动操作")
+            observe.send(value: "Hello")
+            observe.send(value: "World")
+        }
+        
+        let subscriber1 = Observer<String,NoError> { (event) in
+            switch event {
+            case .value(let value):
+                    print("观察者1收到了信号\(value)")
+            default:break
+            }
+        }
+        
+        let subscriber2 = Observer<String,NoError>(value: { print("观察者二收到消息为\($0)")})
+        
+        
+        print("-----1-----")
+        producter.start(subscriber1)
+        // 发生器会再次工作
+        print("-----2-----")
+        producter.start(subscriber2)
+        
+        // 通过管道创建--热信号
+        let (signalA,observerA) = Signal<String,NoError>.pipe()
+        let (signalB,observerB) = Signal<String,NoError>.pipe()
+        
+        observerA.send(value: "A订阅之前")
+        observerB.send(value: "B订阅之前")
+        
+        Signal.combineLatest(signalA, signalB).observeValues { (value1,value2) in
+            print("收到的值\(value1)-\(value2)")
+        }
+        
+        observerA.send(value: "A订阅之后")
+        observerB.send(value: "B订阅之后")
+        
+        observerA.send(value: "A改变了")
+        
+        observerA.sendCompleted()
+        observerB.sendCompleted()
     }
     
+    // MARK: - 文本输入框监听
+    func textFieldDemo() {
+        // 输入时监听
+        textField.reactive.continuousTextValues.observeValues { (text) in
+            print(text ?? "")
+            self.label.text = text ?? "Default"
+            self.label.sizeToFit()
+        }
+    }
+    
+    // MARK: - 基本操作
     func empty() {
         let emptySignal = Signal<Int,NoError>.empty ;
         let observer = Observer<Int,NoError>(
