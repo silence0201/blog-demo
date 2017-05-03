58faf8dba1054cb6cc267c176ca00cb868095362
Silence <374619540@qq.com>
2017-05-03 13:16:21 +0800

    Update

--- Reactive/RxCocoaDemo/RxCocoaDemo/ViewController.swift
+++ Reactive/RxCocoaDemo/RxCocoaDemo/ViewController.swift
@@ -44,6 +44,10 @@ class ViewController: UIViewController {
         skipWhileDemo()
         skipWhileWithIndexDemo()
         skipUntilDemo()
+        
+        toArrayDemo()
+        reduceDemo()
+        concatDemo()
     }
     
     // MARK: - Create
@@ -590,6 +594,54 @@ class ViewController: UIViewController {
         sourceSequence.onNext("🐸")
         sourceSequence.onNext("🐷")
         sourceSequence.onNext("🐵")
+    }
+    
+    // MARK: - Mathematical
+    func toArrayDemo() {
+        print(#function)
+        let disposeBag = DisposeBag()
+        
+        Observable.range(start: 1, count: 10)
+            .toArray()
+            .subscribe { print($0) }
+            .disposed(by: disposeBag)
+    }
+    
+    func reduceDemo() {
+        print(#function)
+        let disposeBag = DisposeBag()
+        
+        Observable.of(10, 100, 1000)
+            .reduce(1, accumulator: +)
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    func concatDemo() {
+        print(#function)
+        let disposeBag = DisposeBag()
+        
+        let subject1 = BehaviorSubject(value: "🍎")
+        let subject2 = BehaviorSubject(value: "🐶")
+        
+        let variable = Variable(subject1)
+        
+        variable.asObservable()
+            .concat()
+            .subscribe { print($0) }
+            .disposed(by: disposeBag)
+        
+        subject1.onNext("🍐")
+        subject1.onNext("🍊")
+        
+        variable.value = subject2
+        
+        subject2.onNext("I would be ignored")
+        subject2.onNext("🐱")
+        
+        subject1.onCompleted()
+        
+        subject2.onNext("🐭")
     }
 }
 
